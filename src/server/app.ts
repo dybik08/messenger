2bcafebe6d0ef2cdbd3141564e0057cb4b68bf94
dybik08 <dybik08@gmail.com>
2018-12-10 17:37:31 +0100

    Connect app to database

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,6 +3,7 @@ import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 import * as path from 'path';
 import { Router } from './controllers/index';
+import * as mongoose from 'mongoose';
 
 class App {
 
@@ -13,6 +14,7 @@ class App {
         this.app = express();
         this.config();
         this.router.routes(this.app);
+        this.handleErrors();
     }
     private config(): void {
         this.app.disable('x-powered-by');
@@ -21,6 +23,19 @@ class App {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
         this.app.use(express.static(path.join(__dirname, '../../dist')));
+
+        mongoose.connect('mongodb://127.0.0.1:27017/chat', {
+            useNewUrlParser: true
+        })
+    }
+
+    private handleErrors() {
+        this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+            const result = { error: err.message };
+            if(this.app.get('env') === 'production')
+                result.error = 'Unable to handle the request';
+            res.status(500).json(result);
+        })
     }
 }
 
