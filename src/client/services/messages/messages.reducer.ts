857ffab854b5158b048d4b64c3de6619a8aa905d
dybik08 <dybik08@gmail.com>
2018-12-13 19:59:43 +0100

    Add messages reducer

---
+++ src/client/services/messages/messages.reducer.ts
@@ -0,0 +1,41 @@
+import { IState, IAction, ACTIONS } from './messages.constants';
+import { Reducer } from 'redux';
+
+export const INITIAL_STATE: IState = {
+    messages: {}
+};
+
+const reducer: Reducer = (state: IState = INITIAL_STATE, action: IAction) => {
+    switch(action.type) {
+        case ACTIONS.RECEIVE_MESSAGE: {
+            state = {
+                ...state,
+                messages: {
+                    ...state.messages,
+                    [action.payload.from]: [
+                        ...(state.messages[action.payload.from] ? state.messages[action.payload.from] : []),
+                        { ...action.payload }
+                    ]
+                }
+            };
+            break;
+        }
+        case ACTIONS.SEND_MESSAGE: {
+            state = {
+                ...state,
+                messages: {
+                    ...state.messages,
+                    [action.payload.to]: [
+                        ...(state.messages[action.payload.to] ? state.messages[action.payload.to] : []),
+                        { ...action.payload }
+                    ]
+                }
+            };
+            break;
+        }
+        default: {}
+    }
+    return state;
+};
+
+export default reducer;
