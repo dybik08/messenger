d180a28c31b86c0b5ad727903f1f9856cc0ad951
dybik08 <dybik08@gmail.com>
2018-12-14 12:35:07 +0100

    Update unit tests. Add user offline test

--- src/client/services/users/users.spec.ts
+++ src/client/services/users/users.spec.ts
@@ -8,15 +8,17 @@ const user: IUser = {
     username: 'john',
     createdAt: Date.now(),
     updatedAt: Date.now(),
-    online: false
+    online: true
 };
 
 const users: Array<IUser> = [user];
 
 describe('users reducer', function() {
+
     it('should return the initial state', () => {
         assert.equal(reducer(undefined, ({} as any)), INITIAL_STATE);
     });
+
     it('should handle REQUEST_USER', () => {
         const action = {
             type: actions.REQUEST_USER,
@@ -24,6 +26,7 @@ describe('users reducer', function() {
         };
         assert.equal(reducer({}, action).isFetching, true);
     });
+
     it('should handle REQUEST_USERS', () => {
         const action = {
             type: actions.REQUEST_USERS,
@@ -31,6 +34,7 @@ describe('users reducer', function() {
         };
         assert.equal(reducer({}, action).isFetching, true);
     });
+
     it('should handle RECEIVE_USER', () => {
         const action = {
             type: actions.RECEIVE_USER,
@@ -38,6 +42,7 @@ describe('users reducer', function() {
         };
         assert.equal(reducer({}, action).users[user._id], action.payload);
     });
+
     it('should handle RECEIVE_USERS', () => {
         const action = {
             type: actions.RECEIVE_USERS,
@@ -45,19 +50,35 @@ describe('users reducer', function() {
         };
         assert.equal(JSON.stringify(reducer({}, action).users), JSON.stringify(mapKeys(action.payload, '_id')));
     });
+
     it('should handle DELETE_USER', () => {
-        let state;
         const action = {
             type: actions.RECEIVE_USER,
             payload: user
         };
-        assert.equal(state = reducer({}, action).users[user._id], action.payload);
+        const state = reducer({}, action);
+        assert.equal(state.users[user._id], action.payload);
         const action2 = {
             type: actions.DELETE_USER,
             payload: user._id
         };
         assert.equal(JSON.stringify(reducer(state, action2).users), JSON.stringify({}));
     });
+
+    it('should handle SET_USER_OFFLINE', () => {
+        const action = {
+            type: actions.RECEIVE_USER,
+            payload: user
+        };
+        const state = reducer({}, action);
+        assert.equal(state.users[user._id], action.payload);
+        const action2 = {
+            type: actions.SET_USER_OFFLINE,
+            payload: user._id
+        };
+        assert.equal(reducer(state, action2).users[user._id].online, false);
+    });
+
     it('should handle SET_USERS_ERRORS', () => {
         const action = {
             type: actions.SET_USERS_ERRORS,
