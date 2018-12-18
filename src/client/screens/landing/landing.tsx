2eba516760624c95a3ab37cf716427f2cbd41ae1
dybik08 <dybik08@gmail.com>
2018-12-18 19:39:57 +0100

    Create landing page

--- src/client/screens/landing/landing.tsx
+++ src/client/screens/landing/landing.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+const styles = require('./landing.scss');
 
 export default () => (
-    <div>
-        <h1>Landing page</h1>
-        <Link to="/signup">Sign up</Link>
-        <Link to="/signin">Sign in</Link>
+    <div className={styles.container}>
+        <div className={styles.wrapper}>
+            <div className={styles.header}>
+                <h1 className={styles.header__title}>Welcome to my <span>messanger app</span></h1>
+                <h2 className={styles.header__subtitle}>Awesome chat app with <span>socket.io</span> web-socket technology</h2>
+                <div className={styles.header__buttons}>
+                    <Link to="/signin">Sign in</Link>
+                    <span>or</span>
+                    <Link to="/signup">Create new account</Link>
+                </div>
+            </div>
+            <img className={styles.picture} src="https://i.imgur.com/vmOOmlO.png" alt="Image"/>
+        </div>
     </div>
 )
